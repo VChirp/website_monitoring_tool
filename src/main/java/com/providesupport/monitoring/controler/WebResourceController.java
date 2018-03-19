e5aaae102e60d161fdf7b5291475c0ed5c109a0e
VChirp <vlad.silchenkov@gmail.com>
2018-03-19 11:01:49 +0200

    fix constructor for beans

--- src/main/java/com/providesupport/monitoring/controler/WebResourceController.java
+++ src/main/java/com/providesupport/monitoring/controler/WebResourceController.java
@@ -7,16 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
 @RequestMapping("/checks")
 public class WebResourceController {
+    private final CheckerService checkerService;
+    private final WebResourceService webResourceService;
+
     @Autowired
-    private CheckerService checkerService;
-    @Autowired
-    private WebResourceService webResourceService;
+    public WebResourceController(CheckerService checkerService, WebResourceService webResourceService) {
+        this.checkerService = checkerService;
+        this.webResourceService = webResourceService;
+    }
 
     @PostMapping
     public WebResource addCheck(@RequestBody WebResource webResource) {
